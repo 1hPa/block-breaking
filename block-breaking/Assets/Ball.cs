bb3763588e1f0b57f2d99a3e10f6981c65d45c12
Yu <tuba0927hy@gmail.com>
2020-08-31 01:28:56 +0900

    feat: add src

---
+++ block-breaking/Assets/Ball.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private float speed = 20.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get rigid body
+        this.GetComponent<Rigidbody>().AddForce(
+            (transform.forward + transform.right) * speed,
+            ForceMode.VelocityChange);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

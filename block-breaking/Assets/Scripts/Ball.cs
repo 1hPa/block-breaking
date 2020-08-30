90f9d81898d9db740cde79e8a885c8291415b64a
Yu <tuba0927hy@gmail.com>
2020-08-31 00:01:55 +0900

    feat: add get rigid body

--- block-breaking/Assets/Scripts/Ball.cs
+++ block-breaking/Assets/Scripts/Ball.cs
@@ -9,7 +9,8 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Get rigid body
+        Rigidbody rb = this.GetComponent<Rigidbody>(); 
     }
 
     // Update is called once per frame

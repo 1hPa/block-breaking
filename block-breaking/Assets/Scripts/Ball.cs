c6096858ed288e7bd6de20cc9d5fb478961faa01
Yu <tuba0927hy@gmail.com>
2020-08-30 22:47:55 +0900

    feat(ball): add initial velocity

--- block-breaking/Assets/Scripts/Ball.cs
+++ block-breaking/Assets/Scripts/Ball.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    // Initial velocity
+    public float speed = 100;
     // Start is called before the first frame update
     void Start()
     {
